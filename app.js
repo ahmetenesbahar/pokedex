154c0551328b38ec3d1d1dbbf0fc32589b07fbdf
ahmetenesbahar <enesbahar2303@gmail.com>
2022-09-22 23:21:41 +0300

    pokedex

--- app.js
+++ app.js
@@ -69,3 +69,15 @@ const createPokemonBox = (pokemon) => {
 };
 
 initPokemon();
+
+searchInput.addEventListener("input", function (e) {
+  const pokeNames = document.querySelectorAll(".poke-name");
+  const search = searchInput.value.toLowerCase();
+
+  pokeNames.forEach((pokeName) => {
+    pokeName.parentElement.style.display = "block";
+    if (!pokeName.innerHTML.toLowerCase().includes(search)) {
+      pokeName.parentElement.style.display = "none";
+    }
+  });
+});
